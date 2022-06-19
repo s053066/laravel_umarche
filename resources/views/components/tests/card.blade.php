aac58161030b435e51d2f75b975781267365188e
katsumaro <ituv2@yahoo.co.jp>
2022-06-19 18:03:03 +0900

    Component初期値指定

--- resources/views/components/tests/card.blade.php
+++ resources/views/components/tests/card.blade.php
@@ -1,3 +1,12 @@
+@props(
+  [
+    'title',
+    'content' => '本文初期値です',  
+    'message' => 'メッセージ初期値です',
+  ]
+)
+
+
 <div class="border-2 shadow-lg w-1/4 p-2">
   <div>{{ $title }}</div>
   <div>画像</div>
--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,6 +1,8 @@
+
 <x-tests.app>
   <x-slot name="header">ヘッダー1</x-slot>
 テスト１
 
   <x-tests.card title="タイトル1" content="本文1" :message="$message" />
+  <x-tests.card title="タイトル2" />
 </x-tests.app>
